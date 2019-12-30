e451719e5c007e31bfe70b1f3394d6870907bdbf
monu18 <mozis.bot@gmail.com>
2019-12-30 16:54:10 +0530

    Atom

---
+++ defanging-an-ip-address.java.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+class Solution {
+    public String defangIPaddr(String address) {
+       // String ss=new String();
+        int len = address.length();
+       	//address=address.substring(0,i)+'['+address.substring(i);
+       	//address=address.replace('.', '[.]');
+        for (int i = 0; i < address.length(); i++) {
+			if (address.charAt(i)=='.') {
+				address=address.substring(0,i)+"[.]"+address.substring(i+1);
+				i++;
+			}
+		}
+        return address;
+
+    }
+}
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String address=sc.nextLine();
+		Solution karn=new Solution();
+		System.out.println(karn.defangIPaddr(address));
+	}
+
+}
