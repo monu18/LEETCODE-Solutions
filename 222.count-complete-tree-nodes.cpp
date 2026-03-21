60cbd8be2305848841e4f9554f0c87e3fbf88687
monu18 <mozis.bot@gmail.com>
2026-03-20 19:33:00 -0500

    222

---
+++ 222.count-complete-tree-nodes.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=222 lang=cpp
+ *
+ * [222] Count Complete Tree Nodes
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+
+        int lh = 0, rh = 0;
+        TreeNode* l = root;
+        TreeNode* r = root;
+
+        while(l) {
+            lh++;
+            l = l->left;
+        }
+
+        while(r) {
+            rh++;
+            r = r->right;
+        }
+
+        if(lh == rh) return (1 << lh) - 1;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+
+    }
+};
+// @lc code=end
+
